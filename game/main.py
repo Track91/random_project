81059a5e2767881ad06c7049b380d4d5690f6804
Track91 <108593642+Track91@users.noreply.github.com>
2022-07-07 21:16:11 +0200

    test

--- game/main.py
+++ game/main.py
@@ -3,50 +3,6 @@ from game import Game
 
 import random
 
-class CometFallEvent:
-
-    def __init__(self, game):
-        self.percent = 0
-        self.percent_speed = 100
-        self.game = game
-
-        self.all_comets = pygame.sprite.Group()
-
-    def add_percent(self):
-        self.percent += 1
-
-    def is_full_loaded(self):
-        return self.percent >= 100
-
-    def reset_percent(self):
-        self.percent = 0
-
-    def meteor_fall(self):
-        pass
-
-    def attenmpt_fall(self):
-        if self.is_full_loaded():
-            print('Pluie de cometes')
-            self.meteor_fall()
-            self.reset_percent()
-
-    def update_bar(self, surface):
-        self.add_percent()
-
-        pygame.draw.rect(surface, (0, 0, 0), [
-            0,
-            surface.get_height() - 20,
-            surface.get_width(),
-            10
-        ])
-
-        pygame.draw.rect(surface, (255, 0, 0), [
-            0,
-            surface.get_height() - 20,
-            (surface.get_width() / 100) * 10,
-            10
-        ])
-
 
 class Game:
     def __init__(self):
