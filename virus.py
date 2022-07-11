1dea2b35e9fc9920e37f41b0267f8e2fed71b773
Track91 <108593642+Track91@users.noreply.github.com>
2022-07-12 00:43:53 +0200

    ajout fichier vir

---
+++ virus.py
@@ -0,0 +1,22 @@
+import os
+'''for i in range(10):
+    path = 'C:/Users/antoi/Desktop/pourtest/test' + str(i) + '.vbs'
+    with open(path, 'w+') as file:
+        file.write('do \n'
+                   'msgbox"Virus detecté ! "\n'
+                   'loop')
+        file.close()'''
+while True:
+    with open('C:/Users/antoi/Desktop/prout.vbs', 'w+') as file:
+        file.write('do \n'
+                   'msgbox"Virus detecté ! "\n'
+                   'loop')
+        file.close()
+        ''
+    if file.close():
+        with open('C:/Users/antoi/Desktop/prout.vbs', 'w+') as file:
+            file.write('do \n'
+                       'msgbox"Virus detecté ! "\n'
+                       'loop')
+            file.close()
+            ''
